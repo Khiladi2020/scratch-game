439a0a1c993ab8d153daab1cdeb28721dd199315
Ravinder Singh <ravinder.singh@groww.in>
2024-08-29 00:38:39 +0530

    chore: measure the exact height & width of the canvas

--- components/game/GameCanvas.tsx
+++ components/game/GameCanvas.tsx
@@ -7,14 +7,39 @@ import Animated, {
 } from "react-native-reanimated";
 import Sprite from "./Sprite";
 import GameContext from "@/context/GameContext";
+import { useState } from "react";
 
 export default function GameCanvas() {
     const { height, width } = useWindowDimensions();
+    const [containerDimensions, setContainerDimensions] = useState<{
+        height: number;
+        width: number;
+    } | null>(null);
+
     const containerMaxHeight = height * 0.6;
 
     return (
-        <GameContext.Provider value={{ height: containerMaxHeight, width }}>
-            <View style={[styles.container, { height: containerMaxHeight }]}>
+        <GameContext.Provider
+            value={{
+                height: containerDimensions?.height ?? 0,
+                width: containerDimensions?.width ?? 0,
+            }}
+        >
+            <View
+                style={[styles.container, { height: containerMaxHeight }]}
+                onLayout={(e) => {
+                    // measure the container dimensions
+                    if (containerDimensions == null) {
+                        const height = e.nativeEvent.layout.height;
+                        const width = e.nativeEvent.layout.width;
+
+                        setContainerDimensions({
+                            height,
+                            width,
+                        });
+                    }
+                }}
+            >
                 <Sprite />
                 <Sprite />
             </View>
