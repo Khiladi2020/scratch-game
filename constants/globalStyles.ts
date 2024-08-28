f1eea7c7dbc23aef76275a408e7b52039edc0269
Ravinder Singh <ravinder.singh@groww.in>
2024-08-29 01:33:51 +0530

    chore: add global styles

---
+++ constants/globalStyles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+    borderBox: {
+        backgroundColor: "white",
+        borderColor: "#D8D8D8",
+        borderWidth: 2,
+        borderRadius: 8,
+    },
+});
+
+export default globalStyles;
