12ccd5e2ce9513dd691a3f49ae5264a56c748d47
Ravinder Singh <ravindermaan0123@gmail.com>
2024-08-30 14:07:05 +0530

    chore: add state for movements in store

--- store/store.ts
+++ store/store.ts
@@ -1,18 +1,28 @@
 import { INITIAL_SPRITES, SpritesData } from "@/constants/initialSprites";
 import { create } from "zustand";
 
+export type Movement = {
+    spriteName: string;
+    action: { name: string; data: any };
+};
+
 type State = {
     sprites: Array<SpritesData>;
+    movements: Array<Movement>;
 };
 
 type Action = {
     addSprite: (newSprite: SpritesData) => void;
+    addMovement: (newMovement: Movement) => void;
 };
 
 const useAppStore = create<State & Action>((set) => ({
     sprites: INITIAL_SPRITES,
+    movements: [],
     addSprite: (newSprite) =>
         set((state) => ({ sprites: state.sprites.concat(newSprite) })),
+    addMovement: (newMovement) =>
+        set((state) => ({ movements: state.movements.concat(newMovement) })),
 }));
 
 export { useAppStore };
