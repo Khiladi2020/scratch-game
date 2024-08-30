7f443ae6a7350cdb4cd495fed0fd94b0a77f81ba
Ravinder Singh <ravindermaan0123@gmail.com>
2024-08-30 16:59:29 +0530

    chore: add reset & play button

--- components/game/GameCanvas.tsx
+++ components/game/GameCanvas.tsx
@@ -1,4 +1,9 @@
-import { StyleSheet, useWindowDimensions, View } from "react-native";
+import {
+    StyleSheet,
+    TouchableOpacity,
+    useWindowDimensions,
+    View,
+} from "react-native";
 import { ThemedView } from "../ThemedView";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
@@ -10,6 +15,9 @@ import GameContext from "@/context/GameContext";
 import React, { useState } from "react";
 import { SpriteCardProps } from "./SpriteManager";
 import { useAppStore } from "@/store/store";
+import { MaterialIcons } from "@expo/vector-icons";
+import { DEFAULT_ICON_SIZE } from "@/constants/sizes";
+import globalStyles from "@/constants/globalStyles";
 
 interface GameCanvasProps {
     updateCoordinates: (name: string, a: number, b: number) => void;
@@ -23,6 +31,7 @@ export default function GameCanvas(props: GameCanvasProps) {
     } | null>(null);
 
     const sprites = useAppStore((state) => state.sprites);
+    const setAnimationState = useAppStore((state) => state.setAnimationState);
 
     const containerMaxHeight = height * 0.6;
 
@@ -57,6 +66,31 @@ export default function GameCanvas(props: GameCanvasProps) {
                         />
                     );
                 })}
+
+                <TouchableOpacity
+                    style={styles.resetButton}
+                    activeOpacity={0.7}
+                >
+                    <MaterialIcons
+                        name="refresh"
+                        size={DEFAULT_ICON_SIZE}
+                        color={"white"}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.playButton}
+                    activeOpacity={0.7}
+                    onPress={() => {
+                        setAnimationState(true);
+                    }}
+                >
+                    <MaterialIcons
+                        name="play-arrow"
+                        size={DEFAULT_ICON_SIZE}
+                        color={"white"}
+                    />
+                </TouchableOpacity>
             </View>
         </GameContext.Provider>
     );
@@ -68,5 +102,19 @@ const styles = StyleSheet.create({
         borderColor: "#D8D8D8",
         borderWidth: 2,
         borderRadius: 8,
+    },
+    resetButton: {
+        ...globalStyles.iconButton,
+        backgroundColor: "#0F9D58",
+        top: -7,
+        right: -7,
+        padding: 12,
+    },
+    playButton: {
+        ...globalStyles.iconButton,
+        backgroundColor: "#4285F4",
+        top: 50,
+        padding: 12,
+        right: -7,
     },
 });
---
+++ constants/sizes.ts
@@ -0,0 +1,3 @@
+const DEFAULT_ICON_SIZE = 18;
+
+export { DEFAULT_ICON_SIZE };
